06065a1341a6c378398c90c6ac0f8bf068485873
julywaits <134684641+julywaits@users.noreply.github.com>
2025-12-30 16:53:09 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAOnyErMndyq2Lfrj8I0jB0cbTvNpxoGjI",
+  authDomain: "resource-project-84d5d.firebaseapp.com",
+  projectId: "resource-project-84d5d",
+  storageBucket: "resource-project-84d5d.firebasestorage.app",
+  messagingSenderId: "303714677572",
+  appId: "1:303714677572:web:c8b6bccabb947300c41d11"
+};
