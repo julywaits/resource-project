2e07238bdb31edfa06261f1eb42611091d21c1a1
julywaits <134684641+julywaits@users.noreply.github.com>
2025-12-30 16:54:33 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,7 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js';
+
 const firebaseConfig = {
   apiKey: "AIzaSyAOnyErMndyq2Lfrj8I0jB0cbTvNpxoGjI",
   authDomain: "resource-project-84d5d.firebaseapp.com",
@@ -6,3 +10,7 @@ const firebaseConfig = {
   messagingSenderId: "303714677572",
   appId: "1:303714677572:web:c8b6bccabb947300c41d11"
 };
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
